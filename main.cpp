d757472e11ea071c0c7dee1bc0ad2cc1aa85db05
nkue-yst <39930174+nkue-yst@users.noreply.github.com>
2022-08-04 14:21:00 +0900

    Update (2022/08/03)

--- main.cpp
+++ main.cpp
@@ -3,18 +3,22 @@
 #include "plugin_base.hpp"
 #include <dlfcn.h>
 #include <iostream>
+#include <memory>
+
+typedef std::unique_ptr<PluginBase> PluginCreateFunc();
 
 int main(int argc, char** argv)
 {
-    const auto plugin_a_dl = dlopen("libplugin_a.dylib", RTLD_LAZY);
-    auto plugin_a = (CreatePluginFunc*)(dlsym(plugin_a_dl, "create"));
+    const auto pluginA_dl = dlopen("libplugin_a.dylib", RTLD_LAZY);
+    auto pluginA_create = (PluginCreateFunc*)(dlsym(pluginA_dl, "create"));
 
     {
-        const auto& plugin = plugin_a();
+        const auto& plugin = pluginA_create();
+
         plugin->printMessage("test");
     }
 
-    dlclose(plugin_a_dl);
+    dlclose(pluginA_dl);
 }
 
 #endif
--- plugin_base.hpp
+++ plugin_base.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <memory>
 #include <string>
 
 class PluginBase
@@ -15,5 +14,3 @@ public:
 protected:
     std::string class_name = "PluginBase";
 };
-
-typedef std::unique_ptr<PluginBase> CreatePluginFunc();
